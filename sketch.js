0dbdcb6d037d84812453e00cc5961eb5166e95ef
Witt Phillips <witt.phillips@yale.edu>
2024-05-17 00:29:03 -0400

    welcome message

--- sketch.js
+++ sketch.js
@@ -16,6 +16,22 @@ function setup() {
 
 function draw() {
   background(50);
+  
+  if (flock.list.length === 0) {
+    fill(255);
+    textSize(24); // Adjusting text size for better readability
+    textAlign(CENTER, CENTER);
+    textFont('monospace');
+    text("------------- angrier boids ------------", width / 2, height / 2 - 90);
+    text("| * click -> boid                      |", width / 2, height / 2 - 60);
+    text("| * shift + click -> predator          |", width / 2, height / 2 - 30);
+    text("| * space + click -> playable predator |", width / 2, height / 2);
+    text("|   -> arrow key controls!             |", width / 2, height / 2 + 30);
+    text("| ~ shoutout to the boid day 1 julia ~ |", width / 2, height / 2 + 60);
+    text("----------------------------------------", width / 2, height / 2 + 90);
+
+  }
+
   flock.run(foodList);
   foodList.run();
 }
