0b62505904c38d8a136b2cc14c2b4ed60fd81c83
Witt Phillips <witt.phillips@yale.edu>
2024-05-17 00:58:00 -0400

    title slide

--- sketch.js
+++ sketch.js
@@ -16,24 +16,33 @@ function setup() {
 
 function draw() {
   background(50);
+  flock.run(foodList);
+  foodList.run();
   
   if (flock.list.length === 0) {
     fill(255);
-    textSize(24); // Adjusting text size for better readability
-    textAlign(CENTER, CENTER);
+    textSize(16);
     textFont('monospace');
-    text("------------- angrier boids ------------", width / 2, height / 2 - 90);
+    textAlign(CENTER, CENTER);
+
+    // ASCII Art Title
+    text(" _____   __ _ _   _   _        ___       _     _     ", width / 2, height / 2 - 220);
+    text("|___ /  / /(_) |_| |_| | ___  / __\\ ___ (_) __| |___ ", width / 2, height / 2 - 200);
+    text("  |_ \\ / / | | __| __| |/ _ \\/__\\/// _ \\| |/ _` / __|", width / 2, height / 2 - 180);
+    text(" ___) / /__| | |_| |_| |  __/ \\/  \\ (_) | | (_| \\__ \\", width / 2, height / 2 - 160);
+    text("|____/\\____/_|\\__|\\__|_|\\___\\_____/\\___/|_|\\__,_|___/", width / 2, height / 2 - 140);
+
+    // Instructions
+    textSize(24);
+    text("----------------------------------------", width / 2, height / 2 - 90);
     text("| * click -> boid                      |", width / 2, height / 2 - 60);
     text("| * shift + click -> predator          |", width / 2, height / 2 - 30);
-    text("| * space + click -> playable predator |", width / 2, height / 2);
-    text("|   -> arrow key controls!             |", width / 2, height / 2 + 30);
+    text("| * space + click -> playable boid     |", width / 2, height / 2);
+    text("| | arrow key controls!                |", width / 2, height / 2 + 30);
     text("| ~ shoutout to the boid day 1 julia ~ |", width / 2, height / 2 + 60);
     text("----------------------------------------", width / 2, height / 2 + 90);
 
   }
-
-  flock.run(foodList);
-  foodList.run();
 }
 
 
